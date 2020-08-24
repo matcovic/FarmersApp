e8b178dd0b803b3d2d4115fdb29ab1fdc01f2bcb
S,S,Saruar Jahan <40640156+sjalim@users.noreply.github.com>
2020-08-24 11:31:56 +0600

    Information Done!

--- app/src/main/java/com/example/farmersapp/InformationFragment.java
+++ app/src/main/java/com/example/farmersapp/InformationFragment.java
@@ -3,23 +3,33 @@ package com.example.farmersapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import java.util.Objects;
+
 public class InformationFragment extends Fragment {
 
-
-    //widgets Button
-    private LinearLayout click_soilCare, click_seedCare, click_cropNutrition
-            , click_insects, click_technology, click_medicine;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    public static final String CLICKED_INFO_STATUS = "clickedInfoStatus";
+    public static final String CROP_NUTRITION_INFO = "crop_nutrition_info";
+    public static final String SOIL_CARE_INFO = "soil_care_info";
+    public static final String SEED_CARE_INFO = "seed_care_info";
+    public static final String INSECTS_INFO = "insects_info";
+    public static final String TECHNOLOGY_INFO = "technology_info";
+    public static final String PESTICIDES_INFO = "pesticides_info";
+    public static final String FARMING_INSTRUMENT_INFO = "farming_instrument_info";
+
+    Bundle bundle;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -52,18 +62,130 @@ public class InformationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View convertView =  inflater.inflate(R.layout.fragment_information, container, false);
-
+        View convertView = inflater.inflate(R.layout.fragment_information, container, false);
+        bundle = new Bundle();
         //Invoking Button
-        click_soilCare = convertView.findViewById(R.id.click_soilCare);
-        click_seedCare = convertView.findViewById(R.id.click_seedCare);
-        click_cropNutrition = convertView.findViewById(R.id.click_cropNutrition);
-        click_insects = convertView.findViewById(R.id.click_insects);
-        click_technology = convertView.findViewById(R.id.click_technology);
-        click_medicine = convertView.findViewById(R.id.click_medicine);
+        //widgets Button
+        LinearLayout click_soilCare = convertView.findViewById(R.id.click_soilCare);
+        LinearLayout click_seedCare = convertView.findViewById(R.id.click_seedCare);
+        LinearLayout click_cropNutrition = convertView.findViewById(R.id.click_cropNutrition);
+        LinearLayout click_insects = convertView.findViewById(R.id.click_insects);
+        LinearLayout click_technology = convertView.findViewById(R.id.click_technology);
+        LinearLayout click_medicine = convertView.findViewById(R.id.click_medicine);
+        LinearLayout click_farming_instrument_info = convertView.findViewById(R.id.click_farming_instrument_info);
 
+
+        click_cropNutrition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        bundle.putString(CLICKED_INFO_STATUS,CROP_NUTRITION_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        click_insects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    bundle.putString(CLICKED_INFO_STATUS,INSECTS_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
+        click_seedCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString(CLICKED_INFO_STATUS,SEED_CARE_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        click_medicine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    bundle.putString(CLICKED_INFO_STATUS,PESTICIDES_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        click_soilCare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString(CLICKED_INFO_STATUS,SOIL_CARE_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        click_technology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString(CLICKED_INFO_STATUS,TECHNOLOGY_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        click_farming_instrument_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bundle.putString(CLICKED_INFO_STATUS,FARMING_INSTRUMENT_INFO);
+                Fragment fragment =InformationListFragment.newInstance("","");
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
 
 
         return convertView;
     }
+
 }
--- app/src/main/java/com/example/farmersapp/adapter/ListInformation_Adapter.java
+++ app/src/main/java/com/example/farmersapp/adapter/ListInformation_Adapter.java
@@ -1,37 +1,109 @@
 package com.example.farmersapp.adapter;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.farmersapp.InformationListDetailsFragment;
+import com.example.farmersapp.InformationListFragment;
+import com.example.farmersapp.R;
+import com.example.farmersapp.model.InformationItem;
+import com.example.farmersapp.util.GlideApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class ListInformation_Adapter extends RecyclerView.Adapter<ListInformation_Adapter.ViewHolder> {
 
 
+    List<InformationItem> mData;
+    Context mContext;
+    String status;
+    public static final String INFO_DETAILS_BRIEF = "infoDetailsBrief";
+    public static final String INFO_DETAILS_TITLE = "infoDetailsTitle";
+    public static final String INFO_DETAILS_PHOTO = "infoDetailsPhoto";
+    public static final String INFO_STATUS = "infoStatus";
+
+    public ListInformation_Adapter(List<InformationItem> mData, Context mContext, String status) {
+        this.mData = mData;
+        this.mContext = mContext;
+        this.status = status;
+    }
+
+
+    public ListInformation_Adapter() {
+    }
 
     @NonNull
     @Override
     public ListInformation_Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_information,parent,false);
+        return new ViewHolder(layout);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ListInformation_Adapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ListInformation_Adapter.ViewHolder holder, final int position) {
 
+        holder.titleTextView.setText(mData.get(position).getInfoTitle());
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://farmersapp-31e06.appspot.com/"+status+"/").child(mData.get(position).getInfoPhoto()+".jpg");
+
+        GlideApp.with(mContext)
+                .load(storageReference)
+                .centerCrop()
+                .into(holder.infoImageView);
+        holder.infoCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = InformationListDetailsFragment.newInstance("","");
+                FragmentManager fragmentManager =((FragmentActivity) mContext).getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                Bundle bundle = new Bundle();
+                bundle.putString(INFO_DETAILS_BRIEF,mData.get(position).getInfoBrief());
+                bundle.putString(INFO_DETAILS_TITLE, mData.get(position).getInfoTitle());
+                bundle.putString(INFO_DETAILS_PHOTO,  mData.get(position).getInfoPhoto());
+                bundle.putString(INFO_STATUS,status);
+
+                fragment.setArguments(bundle);
+                fragmentTransaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right, R.anim.enter_from_right, R.anim.exit_to_right);
+                fragmentTransaction.replace(R.id.container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView infoImageView;
+        TextView titleTextView;
+        CardView infoCardView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
-
-
+            infoImageView = itemView.findViewById(R.id.informationImage_list);
+            titleTextView = itemView.findViewById(R.id.informationTitle_list);
+            infoCardView = itemView.findViewById(R.id.cardView_listItemInfo);
         }
     }
 }
